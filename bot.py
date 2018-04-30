c2e976df7596ff8c08e55b1ec6d32b1e811d6a76
Versaucey <38393670+Versaucey@users.noreply.github.com>
2018-05-01 00:58:13 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+from discord.utils import find
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "s!")
+
+@client.event
+async def on_ready():
+    print("Susan Bot is ready to demonetise!")
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith("pls beg"):
+        await client.send_message(message.channel,"pls beg")
+        
+    if message.content.upper().startswith("pls beg"):
+    
+        await client.send_message(message.channel,"pls beg")
+
+client.login(process.evn.BOT_TOKEN);
